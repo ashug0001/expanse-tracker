6b4b5118281d5b2cd290d40c1a9ee608b5f77a96
Ashutosh Gupta <ashutoshgupta312@gmail.com>
2020-07-18 13:02:04 +0530

    added errorBoundary and header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "./App.css";
 
+import ErrorBoundary from "./ErrorBoundary";
+const Header = lazy(() => import("./components/Header/Header"));
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ErrorBoundary>
+        <Suspense fallback="loading...">
+          <Header title="Expance Tracker" />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
